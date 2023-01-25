d11e5024f52bdc3d7669e6d5e28d89c0af384d6c
dev-alexandre17 <alexandretec77@gmail.com>
2023-01-24 23:34:01 -0800

    JS: Implementando uma pasta javascript

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function encrypt() {
+    var text = document.querySelector("#texto-entrada");
+    console.log(text.value);
+}
+
+function descrypt() {
+    var text = document.querySelector("#texto-entrada");
+    console.log(text.value);
+}
