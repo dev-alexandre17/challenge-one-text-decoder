3ff7866e3aeb10d95a8609d95596105a6a4e4504
dev-alexandre17 <alexandretec77@gmail.com>
2023-01-25 23:48:07 -0800

    HTML, CSS E JS: Implementando sistema criptografia e descriptografia

--- js/main.js
+++ js/main.js
@@ -1,9 +1,42 @@
+function showResult(valueFinalResult) {
+    const mainOutput = document.querySelector('.main-result');
+    mainOutput.innerHTML = valueFinalResult
+    mainOutput.style.backgroundImage = "none";
+}
+
 function encrypt() {
-    var text = document.querySelector("#texto-entrada");
-    console.log(text.value);
+    
+    var textEncrypt = document.querySelector("#texto-entrada").value;
+    let arrayMatrx = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
+
+    textEncrypt = textEncrypt.toLowerCase();
+
+    for (let i = 0; i < arrayMatrx.length; i++) {
+        if (textEncrypt.includes(arrayMatrx[i][0])) {
+            textEncrypt = textEncrypt.replaceAll(arrayMatrx[i][0], arrayMatrx[i][1]);
+        }
+    }
+
+    event.preventDefault()
+    showResult(textEncrypt);
+    return textEncrypt;
+
 }
 
 function descrypt() {
-    var text = document.querySelector("#texto-entrada");
-    console.log(text.value);
+    var textEncrypt = document.querySelector("#texto-entrada").value;
+    let arrayMatrx = [["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"], ["a, ai"]];
+
+    textEncrypt = textEncrypt.toLowerCase();
+
+    for (let i = 0; i < arrayMatrx.length; i++) {
+        if (textEncrypt.includes(arrayMatrx[i][1])) {
+            textEncrypt = textEncrypt.replaceAll(arrayMatrx[i][1], arrayMatrx[i][0]);
+        }
+    }
+
+    event.preventDefault();
+    showResult(textEncrypt);
+    return textEncrypt;
+
 }
